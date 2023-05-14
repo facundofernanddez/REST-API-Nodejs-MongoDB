395d01063f7a9f6c202d8fde32d599b1bb456eaf
Facundo Fernandez <facundofernanddez@gmail.com>
2023-05-13 21:22:35 -0300

    save new task
    post method save task in tasksapi collection mongodb

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
-import express from "express";
+import express, { json } from "express";
 import TasksRoutes from "./routes/tasks.routes.js";
 
 const app = express();
 
+//settings
 app.set("port", process.env.PORT || 4000);
 
+app.use(json());
+
+//routes
 app.get("/", (req, res) => {
   res.send("welcome");
 });
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
+import Task from "../models/Task.js";
 
 const router = Router();
 
 router.get("/", (req, res) => {
   res.send("tasks");
+});
+
+router.post("/", async (req, res) => {
+  const newTask = new Task({
+    title: req.body.title,
+    description: req.body.description,
+    done: req.body.done,
+  });
+  const taskSaved = await newTask.save();
+  res.json(taskSaved);
 });
 
 export default router;
